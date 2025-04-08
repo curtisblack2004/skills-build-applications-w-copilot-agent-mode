630f9380977e8ef34c7284b996fafcffdfb0a2f7
curtisblack2004 <curtisblack2004@gmail.com>
2025-04-08 22:03:32 +0000

    Fixes to the settings.py file

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l2=5@4%zp=&#4-(n42&#8mi6j2l7o_=i1a&kv)wp@frdz8oxbc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fantastic-pancake-v947pr9rv43xggr-8000.app.github.dev"]
 
 
 # Application definition
